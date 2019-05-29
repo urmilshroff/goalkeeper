a6b9ceff92937f750bea104569ea69af11ad0764
urmilshroff <urmilshroff@gmail.com>
2019-05-29 12:40:36 +0530

    Improved UI

--- lib/goals_page.dart
+++ lib/goals_page.dart
@@ -17,6 +17,14 @@ class _GoalsPageState extends State<GoalsPage> {
             : Brightness.dark);
   }
 
+  Color invertColors() {
+    if (Theme.of(context).brightness == Brightness.light) {
+      return MyColors.dark;
+    } else if (Theme.of(context).brightness == Brightness.dark) {
+      return MyColors.light;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,14 +35,14 @@ class _GoalsPageState extends State<GoalsPage> {
             style: TextStyle(
                 color: MyColors.light,
                 fontWeight: FontWeight.w700,
-                fontSize: 28.0)),
+                fontSize: 24.0)),
       ),
       body: Container(
         child: StaggeredGridView.count(
           crossAxisCount: 2,
-          crossAxisSpacing: 12.0,
-          mainAxisSpacing: 12.0,
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0,
+          padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
           children: <Widget>[
             _buildTile(
               Padding(
@@ -49,22 +57,22 @@ class _GoalsPageState extends State<GoalsPage> {
                         children: <Widget>[
                           Text("Goalkeeper",
                               style: TextStyle(
-                                  color: MyColors.blue, fontSize: 18)),
+                                  color: MyColors.aqua, fontSize: 18)),
                           Text("Hello, World!",
                               style: TextStyle(
-                                  color: MyColors.dark,
+                                  color: invertColors(),
                                   fontWeight: FontWeight.w700,
-                                  fontSize: 30.0))
+                                  fontSize: 28.0))
                         ],
                       ),
                       Material(
-                          color: MyColors.blue,
-                          borderRadius: BorderRadius.circular(50.0),
+                          color: MyColors.aqua,
+                          borderRadius: BorderRadius.circular(25.0),
                           child: Center(
                               child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Icon(Icons.check_circle,
-                                color: MyColors.light, size: 40.0),
+                            padding: const EdgeInsets.all(20.0),
+                            child: Icon(Icons.flag,
+                                color: MyColors.light, size: 25.0),
                           )))
                     ]),
               ),
@@ -85,19 +93,19 @@ class _GoalsPageState extends State<GoalsPage> {
                                   color: MyColors.yellow, fontSize: 18)),
                           Text("Hello, World!",
                               style: TextStyle(
-                                  color: MyColors.dark,
+                                  color: invertColors(),
                                   fontWeight: FontWeight.w700,
-                                  fontSize: 30.0))
+                                  fontSize: 28.0))
                         ],
                       ),
                       Material(
                           color: MyColors.yellow,
-                          borderRadius: BorderRadius.circular(50.0),
+                          borderRadius: BorderRadius.circular(25.0),
                           child: Center(
                               child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Icon(Icons.check_circle,
-                                color: MyColors.light, size: 40.0),
+                            padding: const EdgeInsets.all(20.0),
+                            child: Icon(Icons.flag,
+                                color: MyColors.light, size: 25.0),
                           )))
                     ]),
               ),
@@ -115,33 +123,66 @@ class _GoalsPageState extends State<GoalsPage> {
                         children: <Widget>[
                           Text("Goalkeeper",
                               style: TextStyle(
-                                  color: MyColors.aqua, fontSize: 18)),
+                                  color: MyColors.purple, fontSize: 18)),
                           Text("Hello, World!",
                               style: TextStyle(
-                                  color: MyColors.dark,
+                                  color: invertColors(),
                                   fontWeight: FontWeight.w700,
-                                  fontSize: 30.0))
+                                  fontSize: 28.0))
                         ],
                       ),
                       Material(
-                          color: MyColors.aqua,
-                          borderRadius: BorderRadius.circular(50.0),
+                          color: MyColors.purple,
+                          borderRadius: BorderRadius.circular(25.0),
                           child: Center(
                               child: Padding(
-                            padding: const EdgeInsets.all(15.0),
-                            child: Icon(Icons.check_circle,
-                                color: MyColors.light, size: 40.0),
+                            padding: const EdgeInsets.all(20.0),
+                            child: Icon(Icons.flag,
+                                color: MyColors.light, size: 25.0),
+                          )))
+                    ]),
+              ),
+            ),
+            _buildTile(
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text("Goalkeeper",
+                              style:
+                                  TextStyle(color: MyColors.red, fontSize: 18)),
+                          Text("Hello, World!",
+                              style: TextStyle(
+                                  color: invertColors(),
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 28.0))
+                        ],
+                      ),
+                      Material(
+                          color: MyColors.red,
+                          borderRadius: BorderRadius.circular(25.0),
+                          child: Center(
+                              child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Icon(Icons.flag,
+                                color: MyColors.light, size: 25.0),
                           )))
                     ]),
               ),
             ),
           ],
           staggeredTiles: [
-            StaggeredTile.extent(2, 110.0),
-            StaggeredTile.extent(2, 110.0),
-            StaggeredTile.extent(2, 110.0),
-            StaggeredTile.extent(2, 110.0),
-            StaggeredTile.extent(2, 110.0),
+            StaggeredTile.extent(2, 100),
+            StaggeredTile.extent(2, 100),
+            StaggeredTile.extent(2, 100),
+            StaggeredTile.extent(2, 100),
+            StaggeredTile.extent(2, 100),
           ],
         ),
 //          RaisedButton(
@@ -171,10 +212,12 @@ Widget _buildTile(Widget widgetStuff, {Function() onTapAction}) {
       borderRadius: BorderRadius.circular(10.0),
       shadowColor: MyColors.light,
       child: InkWell(
-          onTap: onTapAction != null
-              ? () => onTapAction()
-              : () {
-                  print("Nothing set");
-                },
-          child: widgetStuff));
+        onTap: onTapAction != null
+            ? () => onTapAction()
+            : () {
+                print("Nothing set");
+              },
+        child: widgetStuff,
+        splashColor: MyColors.light,
+      ));
 }
