424874ffffc262ef7834033727e1d618ddc4b6f3
urmilshroff <urmilshroff@gmail.com>
2019-05-29 13:14:23 +0530

    Small changes

--- lib/goals_page.dart
+++ lib/goals_page.dart
@@ -55,124 +55,19 @@ class _GoalsPageState extends State<GoalsPage> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text("Goalkeeper",
+                          Text("Welcome!",
                               style: TextStyle(
-                                  color: MyColors.aqua, fontSize: 18)),
-                          Text("Hello, World!",
-                              style: TextStyle(
-                                  color: invertColors(),
+                                  color: MyColors.dark,
                                   fontWeight: FontWeight.w700,
-                                  fontSize: 28.0))
+                                  fontSize: 24.0)),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("Start by creating a new goal below.",
+                              style: TextStyle(
+                                  color: MyColors.dark, fontSize: 16)),
                         ],
                       ),
-                      Material(
-                          color: MyColors.aqua,
-                          borderRadius: BorderRadius.circular(25.0),
-                          child: Center(
-                              child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Icon(Icons.flag,
-                                color: MyColors.light, size: 25.0),
-                          )))
-                    ]),
-              ),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Goalkeeper",
-                              style: TextStyle(
-                                  color: MyColors.yellow, fontSize: 18)),
-                          Text("Hello, World!",
-                              style: TextStyle(
-                                  color: invertColors(),
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 28.0))
-                        ],
-                      ),
-                      Material(
-                          color: MyColors.yellow,
-                          borderRadius: BorderRadius.circular(25.0),
-                          child: Center(
-                              child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Icon(Icons.flag,
-                                color: MyColors.light, size: 25.0),
-                          )))
-                    ]),
-              ),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Goalkeeper",
-                              style: TextStyle(
-                                  color: MyColors.purple, fontSize: 18)),
-                          Text("Hello, World!",
-                              style: TextStyle(
-                                  color: invertColors(),
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 28.0))
-                        ],
-                      ),
-                      Material(
-                          color: MyColors.purple,
-                          borderRadius: BorderRadius.circular(25.0),
-                          child: Center(
-                              child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Icon(Icons.flag,
-                                color: MyColors.light, size: 25.0),
-                          )))
-                    ]),
-              ),
-            ),
-            _buildTile(
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("Goalkeeper",
-                              style:
-                                  TextStyle(color: MyColors.red, fontSize: 18)),
-                          Text("Hello, World!",
-                              style: TextStyle(
-                                  color: invertColors(),
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 28.0))
-                        ],
-                      ),
-                      Material(
-                          color: MyColors.red,
-                          borderRadius: BorderRadius.circular(25.0),
-                          child: Center(
-                              child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Icon(Icons.flag,
-                                color: MyColors.light, size: 25.0),
-                          )))
                     ]),
               ),
             ),
@@ -193,10 +88,19 @@ class _GoalsPageState extends State<GoalsPage> {
 //          ),
 //        ],
       ),
+//      floatingActionButton: FloatingActionButton.extended(
+//        onPressed: changeBrightness,
+//        icon: Icon(Icons.brightness_2),
+//        label: Text("LIGHTS"),
+//        foregroundColor: MyColors.light,
+//        shape: RoundedRectangleBorder(
+//            borderRadius: BorderRadius.all(Radius.circular(15.0))),
+//        elevation: 5.0,
+//      ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: changeBrightness,
-        icon: Icon(Icons.brightness_2),
-        label: Text("LIGHTS"),
+        onPressed: null,
+        icon: Icon(Icons.add),
+        label: Text("NEW GOAL"),
         foregroundColor: MyColors.light,
         shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(15.0))),
@@ -208,6 +112,7 @@ class _GoalsPageState extends State<GoalsPage> {
 
 Widget _buildTile(Widget widgetStuff, {Function() onTapAction}) {
   return Material(
+      color: Colors.white,
       elevation: 10.0,
       borderRadius: BorderRadius.circular(10.0),
       shadowColor: MyColors.light,
