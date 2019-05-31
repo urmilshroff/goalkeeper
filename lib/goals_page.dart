3646a3319592168a5b446a1e5e61fc61dc457214
urmilshroff <urmilshroff@gmail.com>
2019-05-31 12:11:49 +0530

    Made a basic item creator

--- lib/goals_page.dart
+++ lib/goals_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:goalkeeper/colors.dart';
 import 'package:goalkeeper/new_goal.dart';
-import 'package:goalkeeper/route_transitions.dart';
 
 import 'package:dynamic_theme/dynamic_theme.dart';
 
@@ -11,34 +10,74 @@ class GoalsPage extends StatefulWidget {
 }
 
 class _GoalsPageState extends State<GoalsPage> {
-  bool showNewGoalAlert = false;
+  List<String> _goalsList = [];
 
-  void changeBrightness() {
-    DynamicTheme.of(context).setBrightness(
-        Theme.of(context).brightness == Brightness.dark
-            ? Brightness.light
-            : Brightness.dark);
-  }
-
-  Color invertColors() {
-    //returns appropriate colors for text visibility
-    if (Theme.of(context).brightness == Brightness.light) {
-      return MyColors.dark;
-    } else if (Theme.of(context).brightness == Brightness.dark) {
-      return MyColors.light;
+  bool isThemeCurrentlyDark() {
+    if (Theme.of(context).brightness == Brightness.dark) {
+      return true; //if currently in dark mode
+    } else {
+      return false;
     }
   }
 
-  _createNewGoal(BuildContext context) async {
-    var goalDetails = new List();
-    goalDetails = await Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => NewGoal()),
-    );
+  void changeBrightness() {
+    DynamicTheme.of(context).setBrightness(
+        isThemeCurrentlyDark() ? Brightness.light : Brightness.dark);
+  } //switch between light & dark modes
 
-    Scaffold.of(context)
-      ..removeCurrentSnackBar()
-      ..showSnackBar(SnackBar(content: Text("$goalDetails[0]")));
+  Color invertColors() {
+    if (isThemeCurrentlyDark()) {
+      return MyColors.light;
+    } else {
+      return MyColors.dark;
+    }
+  } //returns appropriate colors for text visibility
+
+//  _createNewGoal(BuildContext context) async {
+//    var goalDetails = new List();
+//    goalDetails = await Navigator.push(
+//      context,
+//      MaterialPageRoute(builder: (context) => NewGoal()),
+//    );
+//
+//    Scaffold.of(context)
+//      ..removeCurrentSnackBar()
+//      ..showSnackBar(SnackBar(content: Text("$goalDetails[0]")));
+//  }
+
+  void _createNewGoal(BuildContext context) {
+    setState(() {
+      int index = _goalsList.length;
+      _goalsList.add('Item ' + index.toString());
+    });
+  }
+
+  Widget _buildTile(Widget widgetStuff, {Function() onTapAction}) {
+    return Material(
+        elevation: 7.5,
+        borderRadius: BorderRadius.circular(10.0),
+        shadowColor: MyColors.light,
+        child: InkWell(
+          onTap: onTapAction != null
+              ? () => onTapAction()
+              : () {
+                  print("Nothing set");
+                },
+          child: widgetStuff,
+          splashColor: MyColors.light,
+        ));
+  }
+
+  Widget _buildGoalsList() {
+    return ListView.builder(itemBuilder: (context, index) {
+      if (index < _goalsList.length) {
+        return _buildGoalItem(_goalsList[index]);
+      }
+    });
+  }
+
+  Widget _buildGoalItem(String goalText) {
+    return new ListTile(title: new Text(goalText));
   }
 
   @override
@@ -54,99 +93,67 @@ class _GoalsPageState extends State<GoalsPage> {
                 fontSize: 24.0)),
         actions: <Widget>[
           IconButton(
-            icon: Theme.of(context).brightness ==
-                    Brightness.light //if currently in light mode
-                ? Icon(Icons.brightness_2) //use moon icon
-                : Icon(Icons.brightness_7), //otherwise use sun icon
+            icon: isThemeCurrentlyDark()
+                ? Icon(Icons.brightness_7) //use sun icon
+                : Icon(Icons.brightness_2), //use moon icon
+            tooltip: isThemeCurrentlyDark()
+                ? "Switch to light mode"
+                : "Switch to dark mode",
             onPressed: changeBrightness,
-            tooltip: Theme.of(context).brightness == Brightness.light
-                ? "Switch to dark mode"
-                : "Switch to light mode",
           ),
         ],
       ),
-      body: Container(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverGrid(
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  crossAxisSpacing: 100.0,
-                  mainAxisSpacing: 0.0,
-                  childAspectRatio: 4.0),
-              delegate: SliverChildListDelegate(
-                [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: _buildTile(
-                      Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text("Welcome!",
-                                      style: TextStyle(
-                                          color: invertColors(),
-                                          fontWeight: FontWeight.w700,
-                                          fontSize: 22.0)),
-                                  SizedBox(
-                                    height: 5,
-                                  ),
-                                  Text("Start by creating a new goal below.",
-                                      style: TextStyle(
-                                          color: invertColors(), fontSize: 15)),
-                                ],
-                              ),
-                            ]),
-                      ),
-                    ),
-                  ),
-                  showNewGoalAlert == true
-                      ? AlertDialog(
-                          title: Text("ALERT BITCHES"),
-                        )
-                      : Text("NO ALERT"),
+      body: _buildGoalsList(),
+//      Container(
+//        child: CustomScrollView(
+//          slivers: <Widget>[
+//            SliverGrid(
+//              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//                  crossAxisCount: 1,
+//                  crossAxisSpacing: 100.0,
+//                  mainAxisSpacing: 0.0,
+//                  childAspectRatio: 4.0),
+//              delegate: SliverChildListDelegate(
+//                [
 //                  Padding(
 //                    padding: const EdgeInsets.all(10.0),
-//                    child: RaisedButton(
-//                      padding: const EdgeInsets.all(1.0),
-//                      onPressed: changeBrightness,
-//                      color: MyColors.primaryColor,
-//                      splashColor: invertColors(),
-//                      child: Text("LIGHTS"),
-//                      shape: RoundedRectangleBorder(
-//                          borderRadius:
-//                              BorderRadius.all(Radius.circular(10.0))),
+//                    child: _buildTile(
+//                      Padding(
+//                        padding: const EdgeInsets.all(10.0),
+//                        child: Row(
+//                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                            crossAxisAlignment: CrossAxisAlignment.center,
+//                            children: <Widget>[
+//                              Column(
+//                                mainAxisAlignment: MainAxisAlignment.center,
+//                                crossAxisAlignment: CrossAxisAlignment.start,
+//                                children: <Widget>[
+//                                  Text("Welcome!",
+//                                      style: TextStyle(
+//                                          color: invertColors(),
+//                                          fontWeight: FontWeight.w700,
+//                                          fontSize: 22.0)),
+//                                  SizedBox(
+//                                    height: 5,
+//                                  ),
+//                                  Text("Start by creating a new goal below.",
+//                                      style: TextStyle(
+//                                          color: invertColors(), fontSize: 15)),
+//                                ],
+//                              ),
+//                            ]),
+//                      ),
 //                    ),
 //                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+//                ],
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           _createNewGoal(context);
-//          if (showNewGoalAlert == true) {
-//            setState(() {
-//              showNewGoalAlert = false;
-//            });
-//          } else if (showNewGoalAlert == false) {
-//            setState(() {
-//              showNewGoalAlert = true;
-//            });
-//          }
-//          Navigator.push(
-//            context,
-//            MaterialPageRoute(builder: (context) => NewGoal()),
-//          );
-//          Navigator.push(context, SizeRoute(page: NewGoal()));
         },
         icon: Icon(Icons.add),
         label: Text("NEW GOAL"),
@@ -157,20 +164,4 @@ class _GoalsPageState extends State<GoalsPage> {
       ),
     );
   }
-}
-
-Widget _buildTile(Widget widgetStuff, {Function() onTapAction}) {
-  return Material(
-      elevation: 7.5,
-      borderRadius: BorderRadius.circular(10.0),
-      shadowColor: MyColors.light,
-      child: InkWell(
-        onTap: onTapAction != null
-            ? () => onTapAction()
-            : () {
-                print("Nothing set");
-              },
-        child: widgetStuff,
-        splashColor: MyColors.light,
-      ));
 }
