b319de9102dfe5319578932253d883a4f7c19cfb
ghaith96 <ghaithjardane@gmail.com>
2019-08-15 13:58:13 +0300

    setup the composition root

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,28 @@
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:goalkeeper/Pages/Home.dart';
+import 'package:goalkeeper/Services/Db.dart';
+import 'package:goalkeeper/Services/Factory.dart';
+import 'package:goalkeeper/Services/GoalsRepository.dart';
+import 'package:goalkeeper/Services/Interfaces/IRepository.dart';
+import 'package:goalkeeper/Services/NotificationCenter.dart';
 import 'package:goalkeeper/Utils/colors.dart';
 
+void main() {
+  prepareDependencies();
   runApp(MyApp(
     repository: Factory().repository,
   ));
+}
+
+// Composition root for our dependencies !
+void prepareDependencies() {
+  var factory = Factory();
+  factory.registerDatabase(Db());
+  factory.registerRepository(GoalsRepository(database: factory.database));
+  factory.registerNotificationCenter(
+      NotificationCenter(repository: factory.repository));
+}
 
 class MyApp extends StatelessWidget {
   final IRepository repository;
